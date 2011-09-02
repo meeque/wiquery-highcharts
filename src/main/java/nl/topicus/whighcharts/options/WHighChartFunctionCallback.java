6b9e7db8f9257802922705a130f5746891c6ae72
Michael Riedel <spam2011@meeque.de>
2011-09-02 23:21:11 +0200

    Marging in more changes I forgot to merge on the last commit. This basically reverts the recent migration of whighcharts to wicket 1.5. We don't need this here, since we want to get rid of wicket anyways.

--- src/main/java/nl/topicus/whighcharts/options/WHighChartFunctionCallback.java
+++ src/main/java/nl/topicus/whighcharts/options/WHighChartFunctionCallback.java
@@ -1,12 +1,13 @@
 package nl.topicus.whighcharts.options;
 
+import java.util.Map;
+
 import nl.topicus.whighcharts.options.jackson.ToStringNoQuoteSerializer;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.RequestCycle;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.request.IRequestParameters;
-import org.apache.wicket.request.cycle.RequestCycle;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @JsonSerialize(using = ToStringNoQuoteSerializer.class)
@@ -25,22 +26,22 @@ public class WHighChartFunctionCallback extends AbstractDefaultAjaxBehavior impl
 	{
 		StringBuilder builder = new StringBuilder();
 		builder.append("function(event){\n ");
-		builder.append(getCallbackScript());
+		builder.append(getCallbackScript(false));
 		builder.append("\n }");
 		return builder.toString();
 	}
 
 	@Override
-	protected CharSequence getCallbackScript()
+	protected CharSequence getCallbackScript(boolean onlyTargetActivePage)
 	{
-		return generateCallbackScript("wicketAjaxGet('" + getCallbackUrl()
+		return generateCallbackScript("wicketAjaxGet('" + getCallbackUrl(onlyTargetActivePage)
 			+ "&' + wHighChartsSerializeEvent(event)");
 	}
 
 	@Override
 	protected void respond(AjaxRequestTarget target)
 	{
-		IRequestParameters map = RequestCycle.get().getRequest().getRequestParameters();
+		Map<String, String[]> map = RequestCycle.get().getRequest().getParameterMap();
 		WHighChartFunctionEvent event = new WHighChartFunctionEvent(map);
 		onEvent(event, target);
 	}
