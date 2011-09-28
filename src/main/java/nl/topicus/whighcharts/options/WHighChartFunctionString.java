c7845e4e44903221c10834b47a09e697ee767018
Michael Riedel <spam2011@meeque.de>
2011-09-28 15:11:30 +0200

    Making WHighChartFunctionString more flexible. Arbitrary function parameters are now supported.

--- src/main/java/nl/topicus/whighcharts/options/WHighChartFunctionString.java
+++ src/main/java/nl/topicus/whighcharts/options/WHighChartFunctionString.java
@@ -10,25 +10,50 @@ public class WHighChartFunctionString implements WHighChartFunction
 	private static final long serialVersionUID = 1L;
 
 	private String functionBody;
+	
+	private String[] paramNames;
 
-	public WHighChartFunctionString(String functionBody)
+	public WHighChartFunctionString( String functionBody )
+	{
+		this( functionBody, new String[] {} );
+	}
+
+	public WHighChartFunctionString( String functionBody, String ... paramNames )
 	{
 		this.functionBody = functionBody;
+		this.paramNames = paramNames;
 	}
 
 	@Override
 	public String toString()
 	{
-		StringBuilder builder = new StringBuilder();
-		builder.append( "function(event){\n " );
+		final StringBuilder builder = new StringBuilder();
+		
+		builder.append( "function( " );
+		for( final String paramName : getParamNames() )
+		{
+			builder.append( paramName ).append( ", " );
+		}
+		if( getParamNames().length >= 1 )
+		{
+			builder.delete( builder.length() - 2, builder.length() );
+		}
+		builder.append( " ) " );
+		
+		builder.append( "{ " );
 		builder.append( getFunctionBody() );
-		builder.append( "\n }" );
+		builder.append( " }" );
 		return builder.toString();
 	}
 
 	public String getFunctionBody()
 	{
 		return functionBody;
+	}
+	
+	public String[] getParamNames()
+	{
+		return paramNames;
 	}
 
 }
